593b8d827028ef3a6810fea2013545be4e75d395
beddiii <48555849+beddiii@users.noreply.github.com>
2021-04-10 15:57:50 +0200

    FIRST COMMIT
    -create deck
    -mix deck
    -give the first cards to both dealer and player
    -select the first move of the play

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const utility = require('./utility.js');
 const readline = require('readline-sync');
+const { stay } = require('./utility.js');
 
 //Number of decks
 const decks = [1, 2, 4, 6, 8];
@@ -11,21 +12,109 @@ var fiches = 1000;
 var dealer = [];
 var countDealer = 0;
 
-//User's cards
-var user = [];
-var countUser = 0;
+//Player's cards
+var player = [];
+var countPlayer = 0;
+
+//Cards pit
+var pit = [];
+var countPit = 0;
+
+//First selection of actions if cards are not equal
+var actionsNoSplit = ["stay", "hit", "double"];
+
+//First selection of actions if cards are not equal
+var actionYesSplit = ["stay", "hit", "double", "split"];
+
+//Fir memorize if the first two cards are equal
+var equal;
+
+//Select the first action that player do
+var firstAction;
+
+//For see if red card is find
+var redCardFound = false;
+
+//See if the hand is finish
+var handFinish = false;
 
 //Ask for how many decks player want and create the complete deck
 var deckNumber = readline.keyInSelect(decks, "How many decks do you want?");
 var deck = utility.createDeck(decks[deckNumber]);
-console.log(deckNumber);
 console.debug(deck.length);
 
 //Shuffle the deck
 utility.deckShuffling(deck);
-console.log(deck);
+//console.log(deck);
 
 //Set the card signal
-switch (deckNumber) {
+utility.signalCard(deck);
+console.log(deck.length);
 
+//Start the game
+while (fiches > 10) {
+	//Select how much fiches play and give the firsts cards to dealer and player
+	start = utility.startHand(fiches, deck, player, countPlayer, dealer, countDealer);
+	countPlayer = start.countP;
+	countDealer = start.countD;
+	redCardFound = start.found;
+
+	if (player[0].value === "A" || player[1].value === "A") {
+		values = utility.countFirstValue(player, "player");
+		console.log("Player's cards: " + player[0].value + player[0].seeds + " " + player[1].value + player[1].seeds + ". Total: " + values.value1 + "/" + values.value2);
+	}
+	else {
+		console.log("Player's cards: " + player[0].value + player[0].seeds + " " + player[1].value + player[1].seeds + ". Total: " + utility.countFirstValue(player, "player"));
+	}
+
+	if (dealer[0].value == "A") {
+		values = utility.countFirstValue(dealer, "dealer");
+		console.log("Dealer's cards: " + dealer[0].value + dealer[0].seeds + ". Total: " + values.value1 + "/" + values.value2);
+	}
+	else {
+		console.log("Dealer's cards: " + dealer[0].value + dealer[0].seeds + ". Total: " + utility.countFirstValue(dealer, "dealer"));
+	}
+
+	//Ask to player what he want to do between: hit, stay, double and
+	if (player[0].weight === player[1].weight) {
+		equal = true;
+		firstAction = readline.keyInSelect(actionYesSplit, "What do you want to do?");
+		console.log(actionYesSplit[firstAction]);
+		switch (actionYesSplit[firstAction]) {
+			case "stay":
+				//INVOCA FUNZIONE PER STARE
+				//utility.stay(dealer, countDealer, deck);
+				break;
+			case "hit":
+				//INVOCA FUNZIONE PER CHIEDERE CARTA
+				break;
+			case "double":
+				//INVOCA FUNZIONE PER RADDOPPIARE
+				break;
+			case "split":
+				//INVOCA FUNZIONE PER DIVIDERE
+				break;
+		}
+	}
+	else {
+		equal = false;
+		firstAction = readline.keyInSelect(actionsNoSplit, "What do you want to do?");
+		console.log(actionsNoSplit[firstAction]);
+		switch (actionYesSplit[firstAction]) {
+			case "stay":
+				//INVOCA FUNZIONE PER STARE
+				break;
+			case "hit":
+				//INVOCA FUNZIONE PER CHIEDERE CARTA
+				break;
+			case "double":
+				//INVOCA FUNZIONE PER RADDOPPIARE
+				break;
+		}
+	}
+
+
+
+
+	break;
 }
--- utility.js
+++ utility.js
@@ -7,19 +7,36 @@ function getRandomInt(min, max) {
 }
 
 module.exports = {
-	//Function for create the complete deck composed by 1, 2, 4, 6, 8 decks of 52 cards
+	//Function for create a complete deck composed by 1, 2, 4, 6, 8 decks of 52 cards
 	createDeck: function(NDeck) {
 		var seeds = ["Picche", "Quadri", "Fiori", "Cuori "];
-		var value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+		var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 		var deck = [];
 		var count = 0;
 
 		for (n = 0; n < NDeck; n++) {
 			for (i = 0; i < seeds.length; i++) {
-				for (j = 0; j < value.length; j++) {
-					deck[count] = {
-						"value": value[j],
-						"seeds": seeds[i]
+				for (j = 0; j < values.length; j++) {
+					if (values[j] === "J" || values[j] === "Q" || values[j] === "K") {
+						deck[count] = {
+							"value": values[j],
+							"seeds": seeds[i],
+							"weight": 10
+						}
+					}
+					else if (values[j] === "A") {
+						deck[count] = {
+							"value": values[j],
+							"seeds": seeds[i],
+							"weight": [1, 11]
+						}
+					}
+					else {
+						deck[count] = {
+							"value": values[j],
+							"seeds": seeds[i],
+							"weight": parseInt(values[j])
+						}
 					}
 					count++;
 				}
@@ -39,5 +56,132 @@ module.exports = {
 			deck[j] = deck[i];
 			deck[i] = help;
 		}
+	},
+
+	//Set signal card
+	signalCard: function(deck) {
+		var countCards = deck.length;
+		var redCardPosition;
+		switch (countCards) {
+			case 52:
+				redCardPosition = getRandomInt(Math.floor(countCards/2) + 10, Math.floor(countCards/2) + 17);
+				break;
+			case 104:
+				redCardPosition = getRandomInt(Math.floor(countCards/2) + 20, Math.floor(countCards/2) + 40);
+				break;
+			case 208:
+				redCardPosition = getRandomInt(Math.floor(countCards/2) + 50, Math.floor(countCards/2) + 80);
+				break;
+			case 312:
+				redCardPosition = getRandomInt(Math.floor(countCards/2) + 90, Math.floor(countCards/2) + 120);
+				break;
+			case 416:
+				redCardPosition = getRandomInt(Math.floor(countCards/2) + 140, Math.floor(countCards/2) + 170);
+				break;
+		}
+
+		deck.splice(redCardPosition, 0, {"value": "RED CARD", "seeds": "JOLLY", "value": null});
+	},
+
+	//Give the first card for start the hand
+	startHand: function(fiches, deck, player, countPlayer, dealer, countDealer) {
+		var found = false;
+
+		//Ask for how many fiches player want to use
+		var fichesPlayed = Number(readline.question("How many fiches do you want to use? (Min. 10 - Max. 50): "));
+		while (Number(fichesPlayed) < 10 || Number(fichesPlayed) > 50) {
+			console.log(fichesPlayed);
+			fichesPlayed = Number(readline.question("How many fiches do you want to use? (Min. 10 - Max. 50): "));
+		}
+		fiches = fiches - fichesPlayed;
+		console.log("Remaining fiches: " + fiches);
+
+		//Give the first card to player
+		if (deck[0].value !== "RED CARD") {
+			player[countPlayer] = deck.shift();
+			countPlayer++;
+		}
+		else {
+			found = true;
+			deck.shift();
+			player[countPlayer] = deck.shift();
+			countPlayer++;
+		}
+
+		//Give the first card to dealer
+		if (deck[0].value !== "RED CARD") {
+			dealer[countDealer] = deck.shift();
+			countDealer++;
+		}
+		else {
+			found = true;
+			deck.shift();
+			player[countPlayer] = deck.shift();
+			countPlayer++;
+		}
+
+		//Give the second card to player
+		if (deck[0].value !== "RED CARD") {
+			player[countPlayer] = deck.shift();
+			countPlayer++;
+		}
+		else {
+			found = true;
+			deck.shift();
+			player[countPlayer] = deck.shift();
+			countPlayer++;
+		}
+
+		return {"countP": countPlayer, "countD": countDealer, "redCard": found};
+	},
+
+	//Control and ask for insurance after the dealer have an ace
+	insurance: function(dealer, fiches) {
+		if (dealer[0].value === "A") {
+			askInsurance = readline.keyInYNStrict("Do you want to insurance your play? (Cost 50% of fiches played)");
+			if (askInsurance) {
+				fiches = fiches - Math.floor(fiches/2);
+				console.log("Insurance accepted. Remains " + fiches + " fiches");
+			}
+		}
+		return fiches;
+	},
+
+	//Count value of cards
+	countFirstValue: function(arr, subject) {
+		if (subject === "player") {
+			if (arr[0].value === "A" || arr[1].value ==="A") {
+				if ((arr[0].value === "A" && arr[1].weight === 10) || (arr[0].weight === 10 && arr[0].value === "A")) {
+					return "BLACKJACK!";
+				}
+				else {
+					return {"value1": arr[0].weight[0] + arr[1].weight[1], "value2": arr[0].weight[0] + arr[1].weight[0]};
+				}
+			}
+			else {
+				return arr[0].weight + arr[1].weight;
+			}
+		}
+		else {
+			if (arr[0].value === "A") {
+				return {"value1": arr[0].weight[0], "value2": arr[0].weight[1]};
+			}
+			else {
+				return arr[0].weight;
+			}
+		}
+	},
+
+	//Stay, dealer's turn
+	stay: function(dealer, countDealer, deck) {
+		//Calcola il totale del dealer
+		/*if (dealer[]) {
+			
+		}
+
+		while (total <= 16) {
+			dealer[countDealer] = deck.shift();
+			
+		}*/
 	}
 }
